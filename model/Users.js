24a419a8b7d6e4d2f00823510ff50502ea8f8228
Migue Mendoza <211218@ids.upchiapas.edu.mx>
2022-06-08 20:36:43 -0500

    commit ORM sequelize

---
+++ model/Users.js
@@ -0,0 +1,16 @@
+import {getData} from './db.js';
+import Sequelize from 'sequelize';
+
+
+const User = getData.sequelizeClient.define('users',{
+    id: {type: Sequelize.SMALLINT, primaryKey: true},
+    username: Sequelize.STRING,
+    email: Sequelize.STRING,
+    password: Sequelize.STRING,
+    phone_number: Sequelize.STRING,
+},{
+    tableName: 'users'
+});
+
+export const getUser = User;
+
