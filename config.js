1355127a422438fe9952184e777dc35e117eb75d
Migue Mendoza <211218@ids.upchiapas.edu.mx>
2022-06-09 12:34:40 -0500

    first commit ORM CRUD

---
+++ config.js
@@ -0,0 +1,10 @@
+export const api = {
+    port: process.env.API_PORT || 4000,
+};
+export const db = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'CSDB',
+    password: '2002',
+    port: '5432',
+};
